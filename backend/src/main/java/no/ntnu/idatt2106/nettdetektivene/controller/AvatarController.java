c49a9c47926b2c9529d0488698f81d7b57d1a7d8
Thomas Oliver Wallin Higgins <tohiggin@stud.ntnu.no>
2026-04-15 11:11:50 +0200

    feat: refactor AvatarController to use Lombok and add security authorization

--- backend/src/main/java/no/ntnu/idatt2106/nettdetektivene/controller/AvatarController.java
+++ backend/src/main/java/no/ntnu/idatt2106/nettdetektivene/controller/AvatarController.java
@@ -3,10 +3,12 @@ package no.ntnu.idatt2106.nettdetektivene.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import no.ntnu.idatt2106.nettdetektivene.dto.avatar.AvatarResponse;
 import no.ntnu.idatt2106.nettdetektivene.dto.avatar.UpdateAvatarRequest;
 import no.ntnu.idatt2106.nettdetektivene.service.AvatarService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,13 +21,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/avatars")
 @Tag(name = "Avatar")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('STUDENT')")
 public class AvatarController {
 
     private final AvatarService avatarService;
-
-    public AvatarController(AvatarService avatarService) {
-        this.avatarService = avatarService;
-    }
 
     @GetMapping("/me")
     @Operation(summary = "Get my avatar, creating a default avatar if missing")
