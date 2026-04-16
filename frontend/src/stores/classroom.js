8d5cd94c2445fba70c2ef323e91bcfda6d287faf
Ola Syrstad Berg <olasy@stud.ntnu.no>
2026-04-16 11:02:57 +0200

    created status fetch action

--- frontend/src/stores/classroom.js
+++ frontend/src/stores/classroom.js
@@ -73,6 +73,18 @@ export const useClassroomStore = defineStore('classroom', () => {
     }
   }
 
+  async function fetchMyStatus(classroomId) {
+    console.log('[classroom] Fetching my status for classroom:', classroomId)
+    try {
+      const { data } = await classroomService.getMyStatus(classroomId)
+      console.log('[classroom] My status:', data.status)
+      return data.status
+    } catch (err) {
+      console.error('[classroom] Failed to fetch status:', err)
+      throw err
+    }
+  }
+
   return {
     classrooms, currentClassroom, students, currentClassroomId,
     fetchMyClassrooms, createClassroom, joinClassroom, fetchStudents, updateStudentStatus
