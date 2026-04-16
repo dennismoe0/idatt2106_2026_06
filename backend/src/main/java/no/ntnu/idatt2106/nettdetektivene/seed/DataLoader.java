3b7e5722231cd50ce9203e1e12c574b9f43bdff3
Christian Petter Remman <cpremman@gmail.com>
2026-04-16 10:36:02 +0200

    feat: add transactional support to DataLoader's run method

--- backend/src/main/java/no/ntnu/idatt2106/nettdetektivene/seed/DataLoader.java
+++ backend/src/main/java/no/ntnu/idatt2106/nettdetektivene/seed/DataLoader.java
@@ -14,6 +14,7 @@ import no.ntnu.idatt2106.nettdetektivene.repository.TaskRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -27,6 +28,7 @@ public class DataLoader implements ApplicationRunner {
     private final ObjectMapper objectMapper;
 
     @Override
+    @Transactional
     public void run(ApplicationArguments args) {
         if (stopRepository.count() > 0) {
             return;
