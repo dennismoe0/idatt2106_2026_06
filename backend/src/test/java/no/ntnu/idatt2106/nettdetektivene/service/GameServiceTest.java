d4ea28ce4df3cfac4065cf2f8bd3b670b79b8cbe
Dennis Moe <dennimoe@stud.ntnu.no>
2026-04-18 15:54:44 +0200

    fix: remove unused anyLong import from GameServiceTest

--- backend/src/test/java/no/ntnu/idatt2106/nettdetektivene/service/GameServiceTest.java
+++ backend/src/test/java/no/ntnu/idatt2106/nettdetektivene/service/GameServiceTest.java
@@ -36,7 +36,6 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
