3a162b37c70a10f8aa514fc6c3479a88351a9c0c
Dennis Moe <dennimoe@stud.ntnu.no>
2026-04-20 11:17:21 +0200

    feat: register /world-map route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -17,6 +17,7 @@ const routes = [
   { path: '/join',          name: 'JoinClassroom',  component: () => import('@/views/student/JoinClassroomView.vue'), meta: { role: 'STUDENT' } },
   { path: '/waiting',       name: 'WaitingRoom',    component: () => import('@/views/student/WaitingRoomView.vue'),   meta: { role: 'STUDENT' } },
   { path: '/map',           name: 'Map',            component: () => import('@/views/student/MapView.vue'),           meta: { role: 'STUDENT', requiresClassroom: true } },
+  { path: '/world-map',     name: 'WorldMap',       component: () => import('@/views/student/WorldMapView.vue'),       meta: { role: 'STUDENT', requiresClassroom: true } },
   { path: '/task',          name: 'Task',           component: () => import('@/views/student/TaskView.vue'),          meta: { role: 'STUDENT', requiresClassroom: true } },
   { path: '/avatar',        name: 'Avatar',         component: () => import('@/views/student/AvatarView.vue'),        meta: { role: 'STUDENT' } },
   { path: '/profil',        name: 'Profile',        component: () => import('@/views/student/ProfileView.vue'),       meta: { role: 'STUDENT' } },
