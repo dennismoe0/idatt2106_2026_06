70a660ea37a9a89985f3dbb87e04065793ff08a4
Dennis Moe <dennimoe@stud.ntnu.no>
2026-04-20 14:00:30 +0200

    feat: classroomService + store deleteClassroom action

--- frontend/src/stores/classroom.js
+++ frontend/src/stores/classroom.js
@@ -100,6 +100,18 @@ export const useClassroomStore = defineStore('classroom', () => {
     }
   }
 
+  async function deleteClassroom(id) {
+    console.log('[classroom] Deleting classroom:', id)
+    try {
+      await classroomService.deleteClassroom(id)
+      classrooms.value = classrooms.value.filter(c => c.id !== id)
+      console.log('[classroom] Deleted classroom:', id)
+    } catch (err) {
+      console.error('[classroom] Failed to delete classroom:', err)
+      throw err
+    }
+  }
+
   async function fetchMyClassroom() {
     console.log('[classroom] Fetching my classroom membership from server')
     try {
@@ -183,6 +195,6 @@ export const useClassroomStore = defineStore('classroom', () => {
 
   return {
     classrooms, currentClassroom, students, currentClassroomId, pendingJoin, displayName, approvalStatus,
-    fetchMyClassrooms, createClassroom, joinClassroom, fetchStudents, fetchMyStatus, updateStudentStatus, fetchMyClassroom, updateMyDisplayName, rehydrate, reset
+    fetchMyClassrooms, createClassroom, joinClassroom, fetchStudents, fetchMyStatus, updateStudentStatus, fetchMyClassroom, updateMyDisplayName, deleteClassroom, rehydrate, reset
   }
 })
