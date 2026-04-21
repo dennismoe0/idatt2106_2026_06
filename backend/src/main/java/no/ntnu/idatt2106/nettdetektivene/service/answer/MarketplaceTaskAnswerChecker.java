10c20036bca1985f35de302989b3c85250f6c15e
Christian Petter Remman <cpremman@gmail.com>
2026-04-21 10:24:49 +0200

    feat: update isCorrect method in MarketplaceTaskAnswerChecker to include Task parameter

--- backend/src/main/java/no/ntnu/idatt2106/nettdetektivene/service/answer/MarketplaceTaskAnswerChecker.java
+++ backend/src/main/java/no/ntnu/idatt2106/nettdetektivene/service/answer/MarketplaceTaskAnswerChecker.java
@@ -1,6 +1,7 @@
 package no.ntnu.idatt2106.nettdetektivene.service.answer;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import no.ntnu.idatt2106.nettdetektivene.entity.Task;
 import no.ntnu.idatt2106.nettdetektivene.entity.TaskType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +20,7 @@ public class MarketplaceTaskAnswerChecker implements TaskAnswerChecker {
     }
 
     @Override
-    public boolean isCorrect(JsonNode correctAnswer, Map<String, Object> answer) {
+    public boolean isCorrect(Task task, JsonNode correctAnswer, Map<String, Object> answer) {
         Object submitted = answer.get("selected");
         if (submitted == null || correctAnswer.path("selected").isMissingNode()) {
             log.warn("[MarketplaceTaskAnswerChecker] MARKETPLACE answer missing 'selected' key");
